9e5339c38519aa253459e2ed53459accbe5bd59a
martian-mindhunter <preseagle13@gmail.com>
2026-09-15 14:36:17 -0700

    Created display function in Main.java & implemented into MainGUI

--- Module/src/main/java/Main.java
+++ Module/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class Main {
 
     static SessionList addNewSession(SessionList sessions, Session sToAdd) {
@@ -6,16 +8,33 @@ public class Main {
             case null -> new SessionList(sToAdd, null);
 
             case SessionList(Session session, SessionList r) -> {
-                    //compareTo checks to see if the current session has an earlier date
-                    if (sToAdd.date().compareTo(session.date()) < 0) {
-                        yield new SessionList(sToAdd, sessions);
-                    } else {
-                        yield new SessionList(session, addNewSession(r, sToAdd));
-                    }
+                //compareTo checks to see if the current session has an earlier date
+                if (sToAdd.date().compareTo(session.date()) < 0) {
+                    yield new SessionList(sToAdd, sessions);
+                } else {
+                    yield new SessionList(session, addNewSession(r, sToAdd));
                 }
-            };
+            }
+        };
+    }
 
-        }
+    static SessionList displayAllSessions(SessionList sessions, JTextArea outputarea){
+        return switch (sessions){
+            case null -> null;
+            case SessionList(Session session, SessionList r) -> {
+                outputarea.append(
+                        "Session ID: " + session.id()
+                        + " | Title: " + session.title()
+                        + " | Mentor: " + session.mentor()
+                        + " | Date: " + session.date()
+                        + " | Location: " + session.location()
+                        + " | Participants: " + session.maxParts()
+                        + "\n"
+                );
+                yield new SessionList(session, displayAllSessions(r, outputarea));
+            }
+        };
+    }
 
     static Session searchByID(SessionList sessions, int target){
         return switch(sessions){
@@ -31,6 +50,8 @@ public class Main {
             }
         };
     }
+
+//    static SessionL
 
     static void main(String[] args) {
 
